1ebbdc65a44866f7ad58cd21eac98d0a74e29fc5
senpai-dxd <senpai.dxdx@gmail.com>
2025-12-15 14:03:11 +0530

    config.env

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 import os
 from os import environ
 
-TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "756596:AAF")
-APP_ID = int(os.environ.get("APP_ID", "2355"))
-API_HASH = os.environ.get("API_HASH", "4vsavkjahvsdhk")
-OWNER_ID = int(os.environ.get("OWNER_ID", "5741918628"))
+TG_BOT_TOKEN = os.environ.get("BOT_TOKEN", "8502803990:AAEft8ANJP10owHzNnzh_bgS0dsVcvFSv6Y")
+APP_ID = int(os.environ.get("APP_ID", "27227762"))
+API_HASH = os.environ.get("API_HASH", "cc9b763a5ebce6e1ed3414c3d805842d")
+OWNER_ID = int(os.environ.get("OWNER_ID", "8201995920"))
 PORT = os.environ.get("PORT", "8080")
-DB_URL = os.environ.get("DB_URI", "mongodb+srv://odb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "Rex_sequencebott")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "S_QV_Sbot")
+DB_URL = os.environ.get("DB_URI", "mongodb+srv://senpaidxdusa_db_user:UkREHZTKMwXMIp6g@cluster0.lvlusw5.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "senpaidxdusa")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Multi_Using_Bot")
 FSUB_PIC = os.environ.get("FSUB_PIC", "https://ibb.co/FL66q5G9")
 START_PIC =os.environ.get("START_PIC", "https://ibb.co/FL66q5G9")
 START_MSG = os.environ.get("START_MSG", "<b>Bᴀᴋᴀᴀᴀ...!!!{mention}</b> \n<blockquote><b><i>Iᴀᴍ ᴀ ᴀᴅᴠᴀɴᴄᴇ sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴅᴠᴀɴᴄᴇ ғᴇᴀᴛᴜʀᴇs. I ᴄᴀɴ sᴇǫᴜᴇɴᴄᴇ ʏᴏᴜʀ ғɪʟᴇs ᴇᴀsɪʟʏ ɪɴ ᴀ sᴇᴄᴏɴᴅ...!!</i></b></blockquote>")
-ABOUT_TXT = os.environ.get("ABOUT_MESSAGE", "<i><b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/RexBots_Official>RexBots</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/RexBots_Official>RexBOTS</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/about_zani/117'>ZANI</a>\n◈ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>ᴍᴏɴɢᴏ ᴅʙ</a>\n» ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/about_zani/117'>ZANI</a></blockquote></b></i>")
+ABOUT_TXT = os.environ.get("ABOUT_MESSAGE", "<i><b><blockquote>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Animex_Studio>Senpai DxD</a>\n◈ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/Animex_Studio>Senpai DxD</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/Animex_Studio/117'>Senpai DxD</a>\n◈ ᴅᴀᴛᴀʙᴀsᴇ: <a href='https://www.mongodb.com/docs/'>ᴍᴏɴɢᴏ ᴅʙ</a>\n» ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href='https://t.me/Animex_Studio/117'>Senpai Dxd</a></blockquote></b></i>")
 HELP_TXT =  os.environ.get("HELP_MESSAGE", "⁉️ Hᴇʟʟᴏ {mention} \n<blockquote expandable><b><i>➪ Iᴀᴍ ᴀ ᴘᴜʙʟɪᴄ ғɪʟᴇ(s) sᴇǫᴜᴇɴᴄᴇ ʙᴏᴛ I ᴄᴀɴ sᴇǫᴜᴇɴᴄᴇ ᴛʜᴇ ғɪʟᴇs ᴀɴᴅ ᴀʟsᴏ I ᴄᴀɴ sᴇɴᴅ ᴛʜᴀᴛ ғɪʟᴇs ɪɴ ᴅᴜᴍᴘ ᴄʜᴀɴɴᴇʟ. </i></b></blockquote>")
 TG_BOT_WORKERS = 10000
 FSUB_LINK_EXPIRY = 300
-DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-10654657458"))
+DATABASE_CHANNEL = int(os.environ.get("DATABASE_CHANNEL", "-1003353295542"))
 LOG_FILE_NAME = "links-sharingbot.txt"
 SEASON_PATTERN = r'[Ss](\d{1,2})'
 EPISODE_PATTERN = r'[Ee][Pp]?(\d{1,3})'
